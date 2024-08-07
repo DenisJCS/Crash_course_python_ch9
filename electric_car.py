9a4277a97da5230277d55c159370102f1101984b
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-08-07 10:57:44 -0400

    Update electric_car.py

--- electric_car.py
+++ electric_car.py
@@ -25,6 +25,24 @@ class Car:
         self.odometer_reading += miles
 
 #Inheritance
+class Battery:
+    """A simple attempt to model a battery for an electric car."""
+    def __init__(self, battery_size = 75):
+        """Initialize the battery's attributes."""
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """Printa statment describing the battery size."""
+        print(f"This car has a {self.battery_size} - kWH battery.")
+
+    def get_range(self):
+        """PRint a statement about the range this battery provides."""
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 150:
+            range = 315
+        print(f"This car can go about {range} miles on a full charge.")
+
 class ElectricCar(Car):
     """
     Initialize attributes of the parent class.
@@ -33,10 +51,20 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         """Initialize attributes of the parent class"""
         super().__init__(make, model, year)
-        self.battery_size = 75
+        self.battery = Battery()
     def describe_battery(self):
         """Print a statment description the battery size"""
+        
         print(f"The car has a battery {self.battery_size} - kWh battery")
+    def fill_gas_tank(self):
+        """Electric car does not have gas tank"""
+        print("This car doesn't have gas tank.")
+
+
 my_tesla = ElectricCar('tesla', 'model s', '2019')
 print(my_tesla.get_descriptive_name())    
-my_tesla.describe_battery()
+my_tesla.battery.describe_battery()
+my_tesla.fill_gas_tank()
+my_tesla.battery.get_range()
+
+
