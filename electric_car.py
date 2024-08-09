45bec51068a25b70f52a8edc27b3cfb8e426c68c
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-08-08 22:36:19 -0400

    Update electric_car.py

--- electric_car.py
+++ electric_car.py
@@ -43,6 +43,12 @@ class Battery:
             range = 315
         print(f"This car can go about {range} miles on a full charge.")
 
+ def upgrade_battery(self):
+        """Check battery and set capacity to 100"""
+        if self.battery_size != 100:
+            self.battery_size = 150
+
+
 class ElectricCar(Car):
     """
     Initialize attributes of the parent class.
@@ -64,7 +70,6 @@ class ElectricCar(Car):
 my_tesla = ElectricCar('tesla', 'model s', '2019')
 print(my_tesla.get_descriptive_name())    
 my_tesla.battery.describe_battery()
-my_tesla.fill_gas_tank()
 my_tesla.battery.get_range()
-
-
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.get_range()
