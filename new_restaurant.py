d702969bcd843815d11184813562aaa39b28ced5
Denis-Jiro <147671249+DenisJCS@users.noreply.github.com>
2024-08-09 22:35:16 -0400

    Update new_restaurant.py

--- new_restaurant.py
+++ new_restaurant.py
@@ -1,1 +1,10 @@
+from restaurant import Restaurant
+
+my_restaurant = Restaurant('Silk road', 'Nomad cuisin')
+my_restaurant.describe_restaurant()
+
+import restaurant
+
+french_restaurant = restaurant.Restaurant('Madam', 'south france')
+french_restaurant.describe_restaurant()
 
